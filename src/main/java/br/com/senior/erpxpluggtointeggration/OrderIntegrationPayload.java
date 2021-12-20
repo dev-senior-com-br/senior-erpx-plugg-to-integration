7d49f0aeddea1f85df5dabe6ac4a9d61572e49e8
Emerson Kopp <15053421+emersonkopp@users.noreply.github.com>
2021-12-20 07:20:17 -0300

    Fixing dtos

--- src/main/java/br/com/senior/erpxpluggtointeggration/OrderIntegrationPayload.java
+++ src/main/java/br/com/senior/erpxpluggtointeggration/OrderIntegrationPayload.java
@@ -5,6 +5,7 @@ import org.apache.camel.component.jackson.JacksonDataFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import br.com.senior.erpx.local.City;
 import br.com.senior.erpx.person.Person;
@@ -18,8 +19,11 @@ public class OrderIntegrationPayload {
 
     public static final JacksonDataFormat ORDER_INTEGRATION_PAYLOAD_FORMAT = new JacksonDataFormat(OrderIntegrationPayload.class);
 
+    @JsonProperty("order")
     public PluggToOrder order;
+    @JsonProperty("city")
     public City city;
+    @JsonProperty("customer")
     public Person customer;
 
 }
